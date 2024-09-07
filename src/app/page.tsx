aeaccdc6640d7b149f8760a028e20b2444c1cf7f
_pooh <natsathon2018@gmail.com>
2024-09-07 18:24:23 +0700

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,41 @@
+
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import { Container, Text, Title,Rating,Textarea,Button,Divider,Group,Pagination} from "@mantine/core";
+import { useState } from 'react';
 
 export default function Home() {
   return (
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
-
+      <Title order={4} mt="md" >Your rating</Title>
+      <Rating value={0} readOnly size="lg"></Rating>
+      <Textarea
+      label="Your review"
+      placeholder="Do you enjoy eating?"
+      mt="md"
+      />
+      <Button variant="filled" color="orange" mt="md">Submit Review</Button>
+      <Divider my="md" />
+      <Group justify="center">
+        <Title order={4}>Elon Musk</Title>
+        <Rating value={5} readOnly />
+      </Group>
+      <Group justify="center">
+        <Text>Best pizza in this world. I give you X score.</Text>
+      </Group>
+      <Divider my="md" />
+      <Group justify="center">
+        <Title order={4}>Mark Zuck</Title>
+        <Rating value={4} readOnly />
+      </Group>
+      <Group justify="center">
+        <Text>My favourite part is pepperoni</Text>
+      </Group>
+      <Group justify="center" mt="md">
+      <Pagination total={20} color="orange" />
+      </Group>
       <Text ta="center" my="sm">
-        Copyright © 2024 Chayanin Suatap 650610560
+        Copyright © 2024 Natsathon Phnamphan 660612145
       </Text>
     </Container>
   );
